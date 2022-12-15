15779318534b37cb37a0c0225d986502d811f9ec
Setkliv <vovakosachev@mail.ru>
2022-12-15 23:09:27 +0300

    Добавьте файлы проекта.

---
+++ Un16.Tests/CalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace Un16.Tests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            calculator.Additional(100, 50);
+            Assert.Equals(150, calculator.Additional(100, 50));
+        }
+
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            calculator.Subtraction(100, 50);
+            Assert.Equals(50, calculator.Subtraction(100, 50)) ;
+        }
+
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            calculator.Miltiplication(100, 50);
+            Assert.Equals(5000, calculator.Miltiplication(100, 50));
+        }
+
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            calculator.Division(100, 50);
+            Assert.Equals(2, calculator.Division(100, 50));
+        }
+    }
+}
